ebc1e049491a834bfeb16dd3639dfafc3e3ad5c6
Daniel Holth <dholth@anaconda.com>
2022-05-12 16:08:58 +0000

    remove duplicate log filter while debugging

--- conda_build/index/__main__.py
+++ conda_build/index/__main__.py
@@ -4,10 +4,15 @@ import logging
 
 # sets up timestamped log lines nicely but can conflict with conda logging by
 # printing messages twice
+# conda resets logging on each subdir
 logging.basicConfig(
     format="%(asctime)s %(message)s",
     datefmt="%Y-%m-%dT%H:%M:%S",
     level=logging.INFO
 )
+logging.getLogger("conda_build.index").setLevel(logging.DEBUG)
 import conda_build.cli.main_index   # must import *after* logging config
+# assert that filtering is happening; remove conda DuplicateFilter
+assert len(logging.getLogger("conda_build.index").filters)
+logging.getLogger("conda_build.index").filters = []
 conda_build.cli.main_index.main()
